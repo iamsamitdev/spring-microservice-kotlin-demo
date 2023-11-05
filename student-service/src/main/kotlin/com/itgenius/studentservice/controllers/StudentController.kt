ad769206342dafdc35990e53f6902187c5d02ada
Samit Koyom <samitkoyom@gmail.com>
2023-11-05 16:36:33 +0700

    update bootRun hot reload

--- student-service/src/main/kotlin/com/itgenius/studentservice/controllers/StudentController.kt
+++ student-service/src/main/kotlin/com/itgenius/studentservice/controllers/StudentController.kt
@@ -15,11 +15,11 @@ class StudentController {
     @GetMapping
     fun getStudents(): List<Student> {
         return listOf(
-            Student(1, "John", 20),
-            Student(2, "Mary", 21),
-            Student(3, "Peter", 22),
-            Student(4, "David", 23),
-            Student(5, "Bob", 24)
+            Student(1, "John McAthee", 20),
+            Student(2, "Mary Crismas", 21),
+            Student(3, "Peter Parker", 22),
+            Student(4, "David Siemon", 23),
+            Student(5, "Bob Malea", 24)
         )
     }
 
